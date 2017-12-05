77ed12ef6a3b63c6c6792370c1aaa784ddc2ced8
Joar <joar.svartholm@gmail.com>
2017-12-05 15:09:45 +0100

    stucture

---
+++ matlab/Argon.m
@@ -0,0 +1,47 @@
+filename='Argon';
+size =16;
+Troom = 22;
+ntab=1.00028209;
+ntab=1+273.15/(273.15+Troom)*(ntab-1);
+
+P = Par(3300:21600);
+V = Var(3300:21600);
+V = V-2.8;
+
+figure;plot(V); hold on
+count = 0;
+ind = 0;
+I = [];
+
+for i = 1:length(V)
+    if V(i)>upper && ind==0
+        ind=1;
+    elseif V(i)<lower && ind ==1
+        count = count+1;
+        ind =0;
+        I = [I i];
+    end
+end
+
+plot(I,V(I),'r*')
+
+
+dP = P(I(3:end));
+dP = dP-dP(1);
+dN = 0:length(dP)-1;
+linFit = fit(dP,dN','poly1')
+fig=figure('Name','fringes vs Pressure'); hold on;grid on;
+meas=plot(dP,dN,'b*');
+Fit= plot(linFit);
+legend([meas Fit],'Measured values','Linear fit','Location','nw');
+xlabel 'Pressure change [kPa]'
+ylabel 'Number of fringes'
+set(gca,'FontSize',size)
+saveas(fig,filename,'png')
+
+alpha = linFit.p1*lambda*Patm/(2*L);
+n = alpha+1;
+
+disp(['Estimated refractive index: ' num2str(n,'%1.8f')])
+disp(['Tabulated value at ' num2str(Troom) ': ' num2str(ntab,'%1.8f')])
+disp(['Error: ' num2str(abs(n-ntab),'%1.8e')])
---
+++ matlab/importdata.m
@@ -0,0 +1,4 @@
+[~,Vai,Pai]=importfile('airMeas.txt');
+[~,Vhe,Phe]=importfile('HeMeas.txt');
+[~,Vni,Pni]=importfile('NMeas.txt');
+[~,Var,Par]=importfile('ArMeas.txt');
