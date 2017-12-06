3080d06170a53cb8f422a5d870f55dac96a4a225
Måns Bermell <bermell92@gmail.com>
2017-12-06 09:48:45 +0100

    first draft setup

--- matlab/Air.m
+++ matlab/Air.m
@@ -1,5 +1,5 @@
 L = 10.245e-2-0.22e-2; %error 0.1e-2
-Patm = 101; %error 0.8
+Patm = 101; %kPa error 0.1
 lambda = 633e-9; %error 0.5e-9
 
 save=0;
@@ -35,6 +35,7 @@ plot(I,V(I),'r*')
 
 dP = P(I(3:end));
 dP = dP-dP(1);
+%dP = dP+randn(length(dP),1);
 dN = 0:length(dP)-1;
 linFit = fit(dP,dN','poly1')
 fig=figure('Name','fringes vs Pressure'); hold on;grid on;
@@ -50,6 +51,8 @@ end
 
 alpha = linFit.p1*lambda*Patm/(2*L);
 n = alpha+1;
+
+
 
 disp(['Estimated refractive index: ' num2str(n,'%1.8f')])
 disp(['Tabulated value at ' num2str(Troom) ': ' num2str(ntab,'%1.8f')])
--- matlab/calibration.m
+++ matlab/calibration.m
@@ -1,12 +1,12 @@
-data=importdata('calibrationdata.xls');
+%data=importdata('calibrationdata.xls');
 
-P = data.data(:,1); %mbar error 0.5
+P = data.data(:,1); %kPa error 0.1
 V = data.data(:,2); %V error 0.05
 
 P = P/10; %kPa error 0.05
 
 
-linFit = fit(V,P,'poly1');
+linFit = fit(V,P,'poly1')
 
 cal = figure('Name','Pressure calibration curve');hold on
 meas = plot(V,P,'*');
@@ -15,5 +15,6 @@ legend([meas Fit],'Measured values','Fitted curve','Location','nw')
 xlabel 'Voltage [V]'
 ylabel 'Pressure [kPa]'
 set(gca,'FontSize',14)
+grid on
 saveas(cal,'calibration','png')
 close all
